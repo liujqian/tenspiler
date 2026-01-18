8752681b24da5755cc28346a8044a89c22450784
liujqian <liujqian@hotmail.com>
2026-01-18 13:18:43 -0800

    Made the new drivers more readable.

--- tenspiler/llama/auto/driver/softmax_part1_driver.py
+++ tenspiler/llama/auto/driver/softmax_part1_driver.py
@@ -3,7 +3,7 @@ from tenspiler.axioms_tenspiler import axioms
 from tenspiler.codegen.utils import DataType
 from tenspiler.tree_parser import analyze_single_loop
 from tenspiler.utils.synthesis_utils import run_synthesis_algorithm
-
+# This driver does not work.
 if __name__ == "__main__":
     driver = Driver()
     driver, input_vars, softmax_part1 = analyze_single_loop(
@@ -11,11 +11,15 @@ if __name__ == "__main__":
         func_name="softmax_part1",
         axioms=axioms,
     )
+
     input_var, max_pos_var = input_vars["input"], input_vars["max_pos"]
+
     driver.add_precondition(input_var.len() > 0)
     driver.add_precondition(max_pos_var <= input_var.len())
     driver.add_precondition(max_pos_var >= 1)
+
     softmax_part1(input_var, max_pos_var)
+
     run_synthesis_algorithm(
         driver=driver,
         data_type=DataType.FLOAT,
--- tenspiler/llama/auto/driver/softmax_part2_driver.py
+++ tenspiler/llama/auto/driver/softmax_part2_driver.py
@@ -4,6 +4,8 @@ from tenspiler.codegen.utils import DataType
 from tenspiler.tenspiler_common import get_map_int_to_int_synth
 from tenspiler.tree_parser import analyze_single_loop
 from tenspiler.utils.synthesis_utils import run_synthesis_algorithm
+
+# This driver does not work.
 if __name__ == "__main__":
     driver = Driver()
     driver, input_vars, softmax_part2 = analyze_single_loop(
@@ -11,13 +13,18 @@ if __name__ == "__main__":
         func_name="softmax_part2",
         axioms=axioms,
     )
-    input_var,max_pos_var ,max_val_var= input_vars["input"], input_vars["max_pos"], input_vars["max_val"]
+
+    input_var, max_pos_var, max_val_var = input_vars["input"], input_vars["max_pos"], input_vars["max_val"]
+
     driver.add_precondition(input_var.len() > 0)
     driver.add_precondition(max_pos_var <= input_var.len())
     driver.add_precondition(max_pos_var >= 1)
+
     map_int_to_int_synth = get_map_int_to_int_synth()
     driver.fns_synths = [map_int_to_int_synth]
+
     softmax_part2(input_var, max_pos_var, max_val_var)
+
     run_synthesis_algorithm(
         driver=driver,
         data_type=DataType.FLOAT,
