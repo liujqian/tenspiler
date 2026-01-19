ef08a39113e6fc0b19515731067ab71b09eaf884
liujqian <liujqian@hotmail.com>
2026-01-18 17:35:43 -0800

    Change a driver failure annotation.

--- tenspiler/c2taco/auto/driver/dsp/matmul_driver.py
+++ tenspiler/c2taco/auto/driver/dsp/matmul_driver.py
@@ -2,7 +2,7 @@ from tenspiler.axioms_tenspiler import axioms
 from tenspiler.codegen.utils import DataType
 from tenspiler.tree_parser import analyze_single_loop, analyze_double_loops
 from tenspiler.utils.synthesis_utils import run_synthesis_algorithm
-# This driver does not work.
+# This driver does not handle so many loops.
 if __name__ == "__main__":
     try:
         driver, input_vars, fun = analyze_single_loop(
