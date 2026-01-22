3bd7e57f022d6aca5c66daab621b3daaebc03154
Xiaoyu Liu <157879676+XiaoyuLiu111@users.noreply.github.com>
2026-01-22 14:26:15 -0800

    Create add_at_coordinates.cc

---
+++ tenspiler/stack_overflow/cpp/for_synthesis/add_at_coordinates.cc
@@ -0,0 +1,27 @@
+#include <vector>
+using namespace std;
+
+vector<vector<int>> add_at_coordinates(vector<vector<int>> coord, vector<int> amounts)
+{
+    vector<vector<int>> total;
+    for (int i = 0; i < 3; i++) {
+        vector<int> row;
+        for (int j = 0; j < 4; j++){
+            row.push_back(0);
+        }
+        total.push_back(row);
+    }
+
+    vector<int> x;
+    vector<int> y;
+    for (int i = 0; i < coord.size(); i++) {
+        x.push_back(coord[i][0]);
+        y.push_back(coord[i][1]);
+    }
+
+    for (int i = 0; i < amounts.size(); i++) {
+        total[x[i]][y[i]] = total[x[i]][y[i]] + amounts[i];
+    }
+
+    return total;
+}
